d5dd229220f29aba977336dc8022a388aa981b92
dickosmad <dickomoahamed@yahoo.fr>
2022-12-11 21:17:29 +0100

    finishing invoice styling and fixing Header, InvoiceList responsiveness

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,54 +1,77 @@
-import React from "react";
+import React, { useState } from "react";
 
 import "./Header.css";
 export default function Header() {
+  const [showFilters, setShowFilters] = useState(false);
   return (
     <header>
       <div className="header-description-group">
         <h2 className="header-title">Invoices</h2>
-        <span className="invoices">There are 7 invoices</span>
+        <p className="invoices">
+          <span className="hidden-at-mobile">There are</span> 7 invoices
+        </p>
       </div>
       <div className="header-flex-group">
         <div className="filters-container">
-          <label>
-            Filter by status{" "}
+          <button
+            aria-label="Filter By Status"
+            className="filter-by-status-btn"
+            onClick={() => setShowFilters((prev) => !prev)}
+          >
+            Filter <span className="hidden-at-mobile">by status</span>{" "}
             <span className="arrow-down">
-              <svg width="11" height="7" xmlns="http://www.w3.org/2000/svg">
-                <path
-                  d="M1 1l4.228 4.228L9.456 1"
-                  stroke="#7C5DFA"
-                  stroke-width="2"
-                  fill="none"
-                  fill-rule="evenodd"
-                />
-              </svg>
+              {showFilters ? (
+                <svg width="11" height="7" xmlns="http://www.w3.org/2000/svg">
+                  <path
+                    d="M1 1l4.228 4.228L9.456 1"
+                    stroke="#7C5DFA"
+                    stroke-width="2"
+                    fill="none"
+                    fill-rule="evenodd"
+                  />
+                </svg>
+              ) : (
+                <svg width="11" height="7" xmlns="http://www.w3.org/2000/svg">
+                  <path
+                    d="M1 1l4.228 4.228L9.456 1"
+                    stroke="#7C5DFA"
+                    stroke-width="2"
+                    fill="none"
+                    fill-rule="evenodd"
+                  />
+                </svg>
+              )}
             </span>
-          </label>
-          <div className="filters">
-            <div className="checkbox-container">
-              <input id="draft" type="checkbox" value="Draft" />
-              <label name="draft" id="draft" htmlFor="draft">
-                Draft
-              </label>
+          </button>
+          {showFilters && (
+            <div className="filters">
+              <div className="checkbox-container">
+                <input id="draft" type="checkbox" value="Draft" />
+                <label name="draft" id="draft" htmlFor="draft">
+                  Draft
+                </label>
+              </div>
+              <div className="checkbox-container">
+                <input id="paid" type="checkbox" value="Paid" />
+                <label name="paid" id="paid" htmlFor="paid">
+                  Paid
+                </label>
+              </div>
+              <div className="checkbox-container">
+                <input id="pending" type="checkbox" value="Pending" />
+                <label name="pending" id="pending" htmlFor="pending">
+                  Pending
+                </label>
+              </div>
             </div>
-            <div className="checkbox-container">
-              <input id="paid" type="checkbox" value="Paid" />
-              <label name="paid" id="paid" htmlFor="paid">
-                Paid
-              </label>
-            </div>
-            <div className="checkbox-container">
-              <input id="pending" type="checkbox" value="Pending" />
-              <label name="pending" id="pending" htmlFor="pending">
-                Pending
-              </label>
-            </div>
-          </div>
+          )}
         </div>
       </div>
-      <button className="add-invoices-container">
-        <span className="add-invoice"><icon>+</icon> </span>
-        New Invoice
+      <button className="add-invoices-btn" aria-label="Add Invoices">
+        <span className="add-invoice">
+          <icon>+</icon>{" "}
+        </span>
+        New <span className="hidden-at-mobile">Invoice</span>
       </button>
     </header>
   );
---
+++ src/components/InvoiceList/Invoice/Invoice.jsx
@@ -0,0 +1,36 @@
+import React from "react";
+
+import "./Invoice.css";
+
+export default function Invoice() {
+  return (
+    <>
+      <ul className="invoice-card">
+        <li className="invoice-id">
+          <span>#</span>RT2123{" "}
+        </li>
+        <li className="invoice-date">Due 12 Oct 2012 </li>
+        <li className="invoice-owner"> Jhon Doe </li>
+        <li className="invoice-price">£102.04 </li>
+        <li className="invoice-status">
+          {" "}
+          <span className="status">
+            <icon className="dot"></icon>
+            Pending
+          </span>
+          <span className="right-arrow">
+            <svg width="7" height="10" xmlns="http://www.w3.org/2000/svg">
+              <path
+                d="M1 1l4 4-4 4"
+                stroke="#7C5DFA"
+                stroke-width="2"
+                fill="none"
+                fill-rule="evenodd"
+              />
+            </svg>
+          </span>
+        </li>
+      </ul>
+    </>
+  );
+}
